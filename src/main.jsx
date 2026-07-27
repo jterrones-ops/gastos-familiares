d654787c5d7d510219c3043ba8ca568d3aad85d5
jterrones-ops <jterrones@elpedregalsa.com>
2026-07-27 14:01:19 -0500

    Forzar comprobación de nuevas versiones

--- src/main.jsx
+++ src/main.jsx
@@ -8,5 +8,8 @@ createRoot(document.getElementById("root")).render(
 );
 
 if ("serviceWorker" in navigator && import.meta.env.PROD) {
-  window.addEventListener("load", () => navigator.serviceWorker.register("/sw.js"));
+  window.addEventListener("load", async () => {
+    const registration = await navigator.serviceWorker.register("/sw.js", { updateViaCache: "none" });
+    await registration.update();
+  });
 }
