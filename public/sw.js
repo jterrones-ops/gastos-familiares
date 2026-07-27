71af2c7f3503bd056c07c9cfb36b23a561961c2b
jterrones-ops <jterrones@elpedregalsa.com>
2026-07-27 13:33:23 -0500

    Add public/sw.js

---
+++ public/sw.js
@@ -0,0 +1,9 @@
+const CACHE = "mi-familia-v1";
+self.addEventListener("install", (event) => {
+  event.waitUntil(caches.open(CACHE).then((cache) => cache.addAll(["/"])));
+});
+self.addEventListener("fetch", (event) => {
+  if (event.request.method === "GET") {
+    event.respondWith(fetch(event.request).catch(() => caches.match(event.request)));
+  }
+});
